54923245a4dc6b97a3a26e304e0930d96f9dcc1c
ajinkyavn1 <ajinkyanarkhede7@gmail.com>
2021-07-13 18:51:13 +0530

    Student Controller Bug Fixed

--- src/main/java/com/Aj/Spring/Controllar/StudentApi.java
+++ src/main/java/com/Aj/Spring/Controllar/StudentApi.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
@@ -14,7 +15,7 @@ public class StudentApi {
     @Autowired
     Student_Service studentService;
     @PostMapping
-    ResponseEntity<Student> insert_Data(Student student){
+    ResponseEntity<Student> insert_Data(@RequestBody  Student student){
         return  new ResponseEntity<Student>(studentService.save(student), HttpStatus.CREATED);
     }
 
