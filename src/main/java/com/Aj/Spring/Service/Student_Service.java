8a68527c8fc7a7e44a8eaeb0996e3bb5d26d873d
ajinkyavn1 <ajinkyanarkhede7@gmail.com>
2021-07-13 20:07:28 +0530

    Student get ALL Student Service

--- src/main/java/com/Aj/Spring/Service/Student_Service.java
+++ src/main/java/com/Aj/Spring/Service/Student_Service.java
@@ -2,7 +2,10 @@ package com.Aj.Spring.Service;
 
 import com.Aj.Spring.Model.Student;
 
+import java.util.List;
+
 public interface Student_Service {
     Student save(Student student);
     void DeleteStudent(long id);
+    List<Student> getAllStudent();
 }
