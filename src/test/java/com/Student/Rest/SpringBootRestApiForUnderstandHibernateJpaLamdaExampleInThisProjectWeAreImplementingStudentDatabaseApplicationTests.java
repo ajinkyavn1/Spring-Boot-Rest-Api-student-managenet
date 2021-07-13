4d754606ff837f0e765deff1b4cc263a96b9b277
ajinkyavn1 <ajinkyanarkhede7@gmail.com>
2021-07-13 18:02:52 +0530

    Project Initialized

---
+++ src/test/java/com/Student/Rest/SpringBootRestApiForUnderstandHibernateJpaLamdaExampleInThisProjectWeAreImplementingStudentDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Student.Rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApiForUnderstandHibernateJpaLamdaExampleInThisProjectWeAreImplementingStudentDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
